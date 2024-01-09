a16c348f67485635689015468815b5482bd5b9ed
andremfreitas <andre.marreiros.freitas@gmail.com>
2024-01-09 15:35:30 +0100

    update with hessian computation as jacobian of grad

--- jax/tutorial/advanced_ad/higher_order.py
+++ jax/tutorial/advanced_ad/higher_order.py
@@ -15,4 +15,33 @@ print(d2fdx(1.))
 print(d3fdx(1.))
 print(d4fdx(1.))
 
+'''
+Hessian
+---------
 
+For the multivariate case, we have that the second order derivative of a function
+is represented by its Hessian matrix.
+
+The Hessian of a real-valued function of several variables, 
+can be identified with the Jacobian of its gradient. JAX provides
+two transformations for computing the Jacobian of a function, jax.jacfwd
+and jax.jacrev, corresponding to forward- and reverse-mode autodiff.
+They give the same answer, but one can be more efficient than the other
+in different circumstances
+
+I guess that if we have a jacobian with many columns (compared to rows),
+then doing foward mode is benefitial, whereas if our jacobian has more 
+rows than columns then reverse ad is benefitial.
+
+'''
+
+
+def hessian(f):
+  return jax.jacfwd(jax.grad(f))
+
+# Let's double check this implementation on the dot product f: x -> x x
+
+def f(x):
+ return jnp.dot(x,x)
+
+print(hessian(f)(jnp.array([1., 2., 3.])))
